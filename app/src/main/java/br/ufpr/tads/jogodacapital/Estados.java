0fbe2f80e5fd6ef3d6d703394fba06df268da439
Gabril <gjperes1@gmail.com>
2022-10-31 16:27:23 -0300

    Melhoria nos testes e na classe de Estado

--- app/src/main/java/br/ufpr/tads/jogodacapital/Estados.java
+++ app/src/main/java/br/ufpr/tads/jogodacapital/Estados.java
@@ -1,39 +1,48 @@
 package br.ufpr.tads.jogodacapital;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public final class Estados {
     private static final Map<String, String> capitais;
+    private static String[] estados;
 
     static {
-        String[][] estados = new String[][] {
-                {"Acre", "Rio Branco"},
-                {"Alagoas", "Maceio"},
-                {"Amazonas", "Manaus"},
-                {"Paraná", "Curitiba"},
-                {"Santa Catarina", "Florianopolis"},
-                {"São Paulo", "Sao Paulo"},
-                {"Rio de Janeiro", "Rio de Janeiro"},
-                {"Minas Gerais", "Belo Horizonte"},
-                {"Rio Grande do Sul", "Porto Alegre"},
-                {"Bahia", "Salvador"},
-                {"Distrito Federal", "Brasilia"},
-                {"Espírito Santo", "Vitoria"},
-                {"Pernambuco", "Recife"},
-                {"Rio Grande do Norte", "Natal"},
-                {"Tocantins", "Palmas"}
+        String[][] dados = new String[][] {
+            {"Acre", "Rio Branco"},
+            {"Alagoas", "Maceio"},
+            {"Amazonas", "Manaus"},
+            {"Paraná", "Curitiba"},
+            {"Santa Catarina", "Florianopolis"},
+            {"São Paulo", "Sao Paulo"},
+            {"Rio de Janeiro", "Rio de Janeiro"},
+            {"Minas Gerais", "Belo Horizonte"},
+            {"Rio Grande do Sul", "Porto Alegre"},
+            {"Bahia", "Salvador"},
+            {"Distrito Federal", "Brasilia"},
+            {"Espírito Santo", "Vitoria"},
+            {"Pernambuco", "Recife"},
+            {"Rio Grande do Norte", "Natal"},
+            {"Tocantins", "Palmas"}
         };
 
         capitais = new HashMap<>();
+        estados = new String[15];
 
+        int i = 0;
         for (String[] estado :
-                estados) {
+                dados) {
             capitais.put(estado[0], estado[1]);
+            estados[i] = estado[0];
+            i++;
         }
     }
 
     public static Map<String, String> getCapitais() {
         return capitais;
     }
+
+    public static String[] getEstados() { return estados; }
 }
--- app/src/test/java/br/ufpr/tads/jogodacapital/UnitTests.java
+++ app/src/test/java/br/ufpr/tads/jogodacapital/UnitTests.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -24,5 +26,25 @@ public class UnitTests {
     public void quantidadeCorretaDeCapitais() {
         Map<String, String> capitais = Estados.getCapitais();
         assertEquals(15, capitais.size());
+    }
+
+    @Test
+    public void quantidadeCorretaDeEstados() {
+        String[] estados = Estados.getEstados();
+        assertEquals(15, estados.length);
+    }
+
+    @Test
+    public void estadosExistem() {
+        String[] estados = Estados.getEstados();
+        assertEquals(true, Arrays.stream(estados).anyMatch(e -> e.equalsIgnoreCase("Paraná")));
+        assertEquals(true, Arrays.stream(estados).anyMatch(e -> e.equalsIgnoreCase("São Paulo")));
+    }
+
+    @Test
+    public void recuperaEstadosCorretamente() {
+        String[] estados = Estados.getEstados();
+        assertEquals("Paraná", estados[3]);
+        assertEquals("Rio Grande do Norte", estados[13]);
     }
 }
