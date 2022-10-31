3c5d7449aba25a336f802b1b847ac3a008fef785
Gabril <gjperes1@gmail.com>
2022-10-30 23:57:50 -0300

    WIP: Ideia inicial da MainActivity

--- app/src/main/java/br/ufpr/tads/jogodacapital/MainActivity.java
+++ app/src/main/java/br/ufpr/tads/jogodacapital/MainActivity.java
@@ -4,11 +4,27 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+    protected final Map<String, String> capitais = Estados.getCapitais();
+    protected int pontuacao = 0;
+    protected int qtdPerguntasRealizadas = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void registrarAcerto() {
+        this.pontuacao += 10;
+    }
+
+    private void realizarPergunta() {
+        if (qtdPerguntasRealizadas < 5) {
+
+            this.qtdPerguntasRealizadas += 1;
+        }
     }
 }
