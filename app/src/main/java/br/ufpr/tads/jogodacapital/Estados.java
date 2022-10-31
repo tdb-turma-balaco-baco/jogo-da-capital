58f8fa791a4a1cfb2bb9b45f1f22b4c86569ff36
Gabril <gjperes1@gmail.com>
2022-10-31 12:09:26 -0300

    Fix da implementação de Estados

--- app/src/main/java/br/ufpr/tads/jogodacapital/Estados.java
+++ app/src/main/java/br/ufpr/tads/jogodacapital/Estados.java
@@ -1,27 +1,37 @@
 package br.ufpr.tads.jogodacapital;
 
+import java.util.HashMap;
 import java.util.Map;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public final class Estados {
-    private static final Map<String, String> capitais = Stream.of(new String[][] {
-            {"Acre", "Rio Branco"},
-            {"Alagoas", "Maceio"},
-            {"Amazonas", "Manaus"},
-            {"Paraná", "Curitiba"},
-            {"Santa Catarina", "Florianopolis"},
-            {"São Paulo", "Sao Paulo"},
-            {"Rio de Janeiro", "Rio de Janeiro"},
-            {"Minas Gerais", "Belo Horizonte"},
-            {"Rio Grande do Sul", "Porto Alegre"},
-            {"Bahia", "Salvador"},
-            {"Distrito Federal", "Brasilia"},
-            {"Espírito Santo", "Vitoria"},
-            {"Pernambuco", "Recife"},
-            {"Rio Grande do Norte", "Natal"},
-            {"Tocantins", "Palmas"}
-    }).collect(Collectors.toMap(k -> (String) k[0], v -> (String) v[1]));
+    private static final Map<String, String> capitais;
+
+    static {
+        String[][] estados = new String[][] {
+                {"Acre", "Rio Branco"},
+                {"Alagoas", "Maceio"},
+                {"Amazonas", "Manaus"},
+                {"Paraná", "Curitiba"},
+                {"Santa Catarina", "Florianopolis"},
+                {"São Paulo", "Sao Paulo"},
+                {"Rio de Janeiro", "Rio de Janeiro"},
+                {"Minas Gerais", "Belo Horizonte"},
+                {"Rio Grande do Sul", "Porto Alegre"},
+                {"Bahia", "Salvador"},
+                {"Distrito Federal", "Brasilia"},
+                {"Espírito Santo", "Vitoria"},
+                {"Pernambuco", "Recife"},
+                {"Rio Grande do Norte", "Natal"},
+                {"Tocantins", "Palmas"}
+        };
+
+        capitais = new HashMap<>();
+
+        for (String[] estado :
+                estados) {
+            capitais.put(estado[0], estado[1]);
+        }
+    }
 
     public static Map<String, String> getCapitais() {
         return capitais;
